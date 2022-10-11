21675d6a8fb251c6b636e3dff443778a40fc05be
Neutronium <84627588+Neutroniumcc@users.noreply.github.com>
2022-10-11 04:01:27 +0330

    Add files via upload

---
+++ kite/precache-manifest.4086971fd8c94f8a8ba8588c3d5ec274.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a463c299534ad015c0cb",
+    "url": "/static/css/main.be80614f.chunk.css"
+  },
+  {
+    "revision": "a463c299534ad015c0cb",
+    "url": "/static/js/main.a463c299.chunk.js"
+  },
+  {
+    "revision": "d918f5b441fd3d06017d",
+    "url": "/static/css/1.5f8e802d.chunk.css"
+  },
+  {
+    "revision": "d918f5b441fd3d06017d",
+    "url": "/static/js/1.d918f5b4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6587c6a6b6ac1ed1b4f2d6fc6f627a69",
+    "url": "/static/media/avatar.6587c6a6.jpg"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "/static/media/photo.d6cbd8e6.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/icon.d41d8cd9.svg"
+  },
+  {
+    "revision": "ef78811f02ee3bdf5318101e58414c53",
+    "url": "/static/media/logo.ef78811f.png"
+  },
+  {
+    "revision": "1ab27afd9f571310358f8b69e1e7db61",
+    "url": "/static/media/backgroud.1ab27afd.png"
+  },
+  {
+    "revision": "fdb0b30a5d3a215e02112075d96f2a46",
+    "url": "/index.html"
+  }
+];

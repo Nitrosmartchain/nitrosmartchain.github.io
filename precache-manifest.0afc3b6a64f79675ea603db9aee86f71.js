55a8ebfb4849e53b8e63305894e31e246c524d2b
NitroChain <84627588+Nitrosmartchain@users.noreply.github.com>
2025-01-02 18:55:22 +0330

    Add files via upload

---
+++ precache-manifest.0afc3b6a64f79675ea603db9aee86f71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfdd12544165a8a9885f02533e5fe29d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5388acfc7a4b76d725b3",
+    "url": "/static/css/main.14686620.chunk.css"
+  },
+  {
+    "revision": "2993a1aecfc4081562ad",
+    "url": "/static/js/2.02a68f68.chunk.js"
+  },
+  {
+    "revision": "679dc0b0298984b03ab372d214526e9a",
+    "url": "/static/js/2.02a68f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5388acfc7a4b76d725b3",
+    "url": "/static/js/main.9cf3db61.chunk.js"
+  },
+  {
+    "revision": "ae2dfafbfc2365c4ffff",
+    "url": "/static/js/runtime-main.f7c5443e.js"
+  },
+  {
+    "revision": "c12c6e801e1a99d7b0cdfc65db52812f",
+    "url": "/static/media/logo.c12c6e80.png"
+  }
+]);
